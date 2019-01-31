8c302653bbaad464693940adec2fe307107908f7
Scott Stadt <scott.stadt@gmail.com>
2019-01-31 16:02:38 -0500

    working app

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 
 import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { resolvers, typeDefs } from './schema'
 
 const PORT = process.env.PORT || 3500
 const app = express()
 
-app.get('/', function(req, res) {
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  playground: true
+})
+
+server.applyMiddleware({ app })
+
+app.get('/', (req, res) => {
   res.send({ hello: 'there!' })
 })
 
-app.listen(PORT, () => `Listening at http://localhost:${PORT}`)
+app.listen(PORT, () =>
+  console.log(`Listening at http://localhost:${PORT}/graphql`)
+)
